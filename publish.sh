a5b77b2f48c59994a56aeab4243fad48bc5d3701
Aram Kocharyan <akarmenia@gmail.com>
2013-05-28 11:23:59 +1000

    minor changes

--- publish.sh
+++ publish.sh
@@ -11,12 +11,13 @@ TMP_DIR="tmp"
 git checkout $SOURCE_BRANCH
 jekyll build -d $TMP_DIR
 git checkout $DEST_BRANCH
+# This will removed previous files, which we may not want (e.g. CNAME)
 # git rm -qr .
 cp -r $TMP_DIR/. .
-# Delete myself
+# Delete this script from the output
 rm ./$SELF
 rm -r $TMP_DIR
 git add -A
-# git commit -m "Published updates"
-# git push origin master
-# git checkout $SOURCE_BRANCH
+git commit -m "Published updates"
+git push origin master
+git checkout $SOURCE_BRANCH
